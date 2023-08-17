ffa8e7f3b18fd09e95a4756a500019f4b4867f56
Khalid BELHADJ <57879429+K-BEL@users.noreply.github.com>
2023-08-17 23:23:30 +0100

    Add files via upload

--- Section_2/app_api.py
+++ Section_2/app_api.py
@@ -1,7 +1,8 @@
 import json
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from pydantic import BaseModel
 from typing import List
+from fastapi.responses import JSONResponse
 from fastapi.security.api_key import APIKeyHeader
 
 app = FastAPI()
@@ -14,37 +15,46 @@ def authenticate(api_key: str = Depends(api_key_header)):
     if api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
-class DataResponse(BaseModel):
-    data: dict
+# Define your site paths
+site_paths = [
+    r'C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site1.json',
+    r'C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site2.json',
+    r'C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site3.json'
+]
 
-def load_json(json_path):
-    with open(json_path, "r") as json_file:
-        json_data = json.load(json_file)
-        return json_data
+# Model to validate the request body
+class RequestInfo(BaseModel):
+    site_key: int  # Assuming site_key will be an index (1, 2, 3) to select the site
+    data: dict  # You can adjust the data structure based on your needs
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the JSON Data API!"}
-
-@app.get("/data_sets", response_model=List[str])
-def get_data_sets():
-    return ["site1", "site2", "site3"]
-
-@app.get("/data_sets/{data_set}", response_model=DataResponse)
-def get_data(data_set: str, auth: None = Depends(authenticate)):
-    if data_set == "site1":
-        json_path = r"C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site1.json"
-    elif data_set == "site2":
-        json_path = r"C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site2.json"
-    elif data_set == "site3":
-        json_path = r"C:\Users\LENOVO\Desktop\Test_DE\Section_2\Saves\site3.json"
+# POST endpoint to post JSON and select a site
+@app.post("/postJSON")
+async def post_json(info: RequestInfo, authorized: bool = Depends(authenticate)):
+    selected_site_index = info.site_key - 1
+    if 0 <= selected_site_index < len(site_paths):
+        selected_site_path = site_paths[selected_site_index]
+        # Write the posted JSON data to the selected site file
+        with open(selected_site_path, 'w') as site_file:
+            json.dump(info.data, site_file)
+            return {
+                "status": "SUCCESS",
+                "message": f"JSON data posted to site {info.site_key}"
+            }
     else:
-        raise HTTPException(status_code=404, detail="Data set not found")
+        raise HTTPException(status_code=404, detail="Site not found")
+    
 
-    json_data = load_json(json_path)
-    return DataResponse(data=json_data)
-
-# Run the FastAPI application
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# GET endpoint to visualize JSON data of a specific site
+@app.get("/postJSON/visualize/{site_key}")
+async def visualize_data(site_key: int, authorized: bool = Depends(authenticate)):
+    selected_site_index = site_key - 1
+    if 0 <= selected_site_index < len(site_paths):
+        selected_site_path = site_paths[selected_site_index]
+        try:
+            with open(selected_site_path, 'r') as site_file:
+                site_data = json.load(site_file)
+                return JSONResponse(content=site_data, status_code=200)
+        except Exception as e:
+            return JSONResponse(content={"error": str(e)}, status_code=500)
+    else:
+        raise HTTPException(status_code=404, detail="Site not found")
